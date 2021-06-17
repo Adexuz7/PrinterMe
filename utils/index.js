b2e64950739684ab5692144b0b2f9b253c427202
Alvaro Poncio <aponcio@gmail.com>
2021-06-17 10:38:09 +0100

    groups

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 const { GroupsModel } = require('../api/models/groups.model')
 const { UserModel } = require('../api/models/users.model')
+const mongoose = require ('mongoose')
 
 exports.checkAuth = (req, res, next) => {
   jwt.verify(req.headers.token, process.env.SECRET, (err, token) => {
@@ -36,15 +37,19 @@ exports.isAdmin = (req, res, next) => {
 
 exports.isModerator = (req, res, next) => {
   jwt.verify(req.headers.token, process.env.SECRET, (err, token) => {
-    if (err) { res.status(403).json({ error: 'Token not valid' }) }
+    if (err) { return res.status(403).json({ error: 'Token not valid' }) }
     GroupsModel
-      .findOne({ email: token.email })
+      .findOne({ $and: [{ moderator: res.locals.user._id }, { _id: req.params.groupId }] })
       .then(group => {
-        if (group.moderator === res.locals.user._id) {
+        if (group.moderator.equals(res.locals.user.id)) {
           next()
         } else {
           res.json({ err: 'You don\'t have permits' })
         }
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json({ msg: 'Error' })
       })
   })
 }
